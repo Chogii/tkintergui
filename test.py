f53ed310590a09c9043b687507ee417f1de8ee2c
Joseph Shackleford <josephshackleford04@gmail.com>
2022-03-26 01:37:36 -0400

    main

---
+++ test.py
@@ -0,0 +1,40 @@
+from cgi import test
+from hmac import new
+import tkintergui as turtlegui
+
+testElement = turtlegui.Element()
+
+def test(element):
+    element.setcontent('Good Job!')
+    element.render()
+    print("Hello World!")
+    element.onclick('disable')
+
+def test2(element):
+    print("hello world")
+
+testElement.setx(100)
+testElement.sety(100)
+testElement.setwidth(200)
+testElement.setheight(50)
+testElement.setcolor("5069E5")
+testElement.setcontent("Button")
+testElement.settextcolor("FFFFFF")
+testElement.setfontfamily("Segoe UI")
+testElement.onclick(test)
+testElement.render()
+
+testElement2 = turtlegui.Element()
+
+testElement2.setx(200)
+testElement2.sety(200)
+testElement2.setwidth(200)
+testElement2.setheight(100)
+testElement2.setcolor("FF0000")
+testElement2.setcontent("Button")
+testElement2.settextcolor("FFFFFF")
+testElement2.setfontfamily("Segoe UI")
+testElement2.render()
+
+testElement.render()
+testElement.finish()
